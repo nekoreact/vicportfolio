92fae6581848c967c12f5b6d78964024c8705687
nekoreact <happydevcore@outlook.com>
2023-10-12 23:10:14 -0400

    new css and rules2

---
+++ src/components/WhoIAm.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+<template>
+    <p>
+        My name is Victor Alfonso Moreno, I'm a front-end developer based in Caracas, Venezuela. 
+        I have developed many types of front-ends 
+        from well know admin panels to Ecommerce booking platforms.
+    </p>
+    <p>
+        My name is Victor Alfonso Moreno, I'm a front-end developer based in Caracas, Venezuela. 
+        I have developed many types of front-ends 
+        from well know admin panels to Ecommerce booking platforms.
+    </p>
+</template>
