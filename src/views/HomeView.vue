543df0b0a71c9805c431a7792ed7616f7e830d1f
nekoreact <happydevcore@outlook.com>
2023-10-12 23:24:15 -0400

    adding dist d

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HelloWorld from '../components/HelloWorld.vue';
+import WhyILoveDoingCodeVue from '../components/WhyILoveDoingCode.vue';
+</script>
+
+<template>
+    <!-- <TheWelcome /> -->
+    <main>
+    <div class="w-full flex flex-col">
+      <WhyILoveDoingCodeVue></WhyILoveDoingCodeVue>
+      <HelloWorld/>
+    </div>
+
+  </main>
+</template>
